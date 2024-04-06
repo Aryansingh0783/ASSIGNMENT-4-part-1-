78ef7537ef243c70b902c215967c11f7eb0c976a
ARYAN SINGH <95996788+Zerakanian@users.noreply.github.com>
2024-04-06 19:05:29 +0530

    Add files via upload

---
+++ examples/systemcalls/systemcalls.c
@@ -0,0 +1,149 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include "systemcalls.h"
+
+
+/**
+ * @param cmd the command to execute with system()
+ * @return true if the command in @param cmd was executed
+ *   successfully using the system() *call, false if an error occurred,
+ *   either in invocation of the system() call, or if a non-zero return
+ *   value was returned by the command issued in @param cmd.
+*/
+bool do_system(const char *cmd)
+{
+
+/*
+ *  Call the system() function with the command set in the cmd
+ *   and return a boolean true if the system() call completed with success
+ *   or false() if it returned a failure
+*/
+    int ret;
+    ret = system(cmd);
+    return (ret == 0) ? true : false;
+}
+
+/**
+* @param count -The numbers of variables passed to the function. The variables are command to execute.
+*   followed by arguments to pass to the command
+*   Since exec() does not perform path expansion, the command to execute needs
+*   to be an absolute path.
+* @param ... - A list of 1 or more arguments after the @param count argument.
+*   The first is always the full path to the command to execute with execv()
+*   The remaining arguments are a list of arguments to pass to the command in execv()
+* @return true if the command @param ... with arguments @param arguments were executed successfully
+*   using the execv() call, false if an error occurred, either in invocation of the
+*   fork, waitpid, or execv() command, or if a non-zero return value was returned
+*   by the command issued in @param arguments with the specified arguments.
+*/
+
+bool do_exec(int count, ...)
+{
+    va_list args;
+    va_start(args, count);
+    char * command[count+1];
+    int i;
+    for(i=0; i<count; i++)
+    {
+        command[i] = va_arg(args, char *);
+    }
+
+    /* command list must be NULL terminated */
+    command[count] = NULL;
+    va_end(args);
+    /*
+    *   Execute a system command by calling fork, execv(),
+    *   and wait instead of system (see LSP page 161).
+    *   Use the command[0] as the full path to the command to execute
+    *   (first argument to execv), and use the remaining arguments
+    *   as second argument to the execv() command.
+    *
+    */
+
+    int status;
+    pid_t pid;
+    pid = fork();
+
+    if (pid == -1)
+        return false;
+    else if (pid == 0) 
+    {
+        execv (command[0], command); /* will only return on error */
+        exit(EXIT_FAILURE); /* exit child process with failure */
+    }
+
+    pid_t ret_pid;
+    ret_pid = waitpid (pid, &status, 0); /* pid_t waitpid(pid_t pid, int *stat_loc, int options) */
+    if (ret_pid == -1)
+        return false;
+    
+    /* pid == ret_pid ? 
+    * check exit status code
+    */
+    return WIFEXITED(status) && WEXITSTATUS(status) == 0;
+}
+
+/**
+* @param outputfile - The full path to the file to write with command output.
+*   This file will be closed at completion of the function call.
+* All other parameters, see do_exec above
+*/
+bool do_exec_redirect(const char *outputfile, int count, ...)
+{
+    va_list args;
+    va_start(args, count);
+    char * command[count+1];
+    int i;
+    for(i=0; i<count; i++)
+    {
+        command[i] = va_arg(args, char *);
+    }
+    /* command list must be NULL terminated */
+    command[count] = NULL;
+    va_end(args);
+
+    /*
+    *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a reference,
+    *   redirect standard out to a file specified by outputfile.
+    *   The rest of the behaviour is same as do_exec()
+    */
+
+    int fd;
+    /* should we add the O_CLOEXEC option, as the comment in the stackoverflow post suggested?*/
+    fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+
+    if (fd < 0) 
+        return false;
+
+    int status;
+    pid_t pid;
+    pid = fork();
+
+    if (pid == -1)
+        return false;
+    else if (pid == 0) 
+    {
+        if (dup2(fd, 1) < 0)
+            exit(EXIT_FAILURE); /* exit child process with failure */
+        close(fd);
+        execv (command[0], command); /* will only return on error */
+        exit(EXIT_FAILURE); /* exit child process with failure */
+    }
+
+    pid_t ret_pid;
+    ret_pid = waitpid (pid, &status, 0); /* pid_t waitpid(pid_t pid, int *stat_loc, int options) */
+    if (ret_pid == -1)
+        return false;
+    
+    /* pid == ret_pid ? 
+    * check exit status code
+    */
+    return WIFEXITED(status) && WEXITSTATUS(status) == 0;
+
+}
---
+++ finder-app/manual-linux.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+# Script outline to install and build kernel.
+# Author: Siddhant Jajoo.
+
+set -e
+set -u
+
+OUTDIR=/tmp/aeld
+KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
+KERNEL_VERSION=v5.1.10
+BUSYBOX_VERSION=1_33_1
+FINDER_APP_DIR=$(realpath $(dirname $0))
+ARCH=arm64
+CROSS_COMPILE=aarch64-none-linux-gnu-
+
+if [ $# -lt 1 ]
+then
+	echo "Using default directory ${OUTDIR} for output"
+else
+	OUTDIR=$1
+	echo "Using passed directory ${OUTDIR} for output"
+fi
+
+mkdir -p ${OUTDIR}
+
+cd "$OUTDIR"
+if [ ! -d "${OUTDIR}/linux-stable" ]; then
+    #Clone only if the repository does not exist.
+	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
+	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+fi
+if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
+    cd linux-stable
+    echo "Checking out version ${KERNEL_VERSION}"
+    git checkout ${KERNEL_VERSION}
+
+    # Add your kernel build steps here
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    # make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+fi
+
+echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/
+
+echo "Creating the staging directory for the root filesystem"
+cd "$OUTDIR"
+if [ -d "${OUTDIR}/rootfs" ]
+then
+	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
+    sudo rm  -rf ${OUTDIR}/rootfs
+fi
+
+# Create necessary base directories
+mkdir -p "${OUTDIR}/rootfs"
+cd "${OUTDIR}/rootfs"
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
+
+cd "$OUTDIR"
+if [ ! -d "${OUTDIR}/busybox" ]
+then
+git clone git://busybox.net/busybox.git
+    cd busybox
+    git checkout ${BUSYBOX_VERSION}
+    # Configure busybox
+    make distclean
+    make defconfig
+else
+    cd busybox
+fi
+
+# Make and install busybox
+make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make -j4 CONFIG_PREFIX="${OUTDIR}/rootfs" ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+
+echo "Library dependencies"
+cd ${OUTDIR}/rootfs
+${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+
+# Add library dependencies to rootfs
+SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
+
+# Add library dependencies to rootfs
+cp ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
+cp ${SYSROOT}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
+cp ${SYSROOT}/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
+cp ${SYSROOT}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
+
+# Make device nodes
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 600 dev/console c 5 1
+
+# Clean and build the writer utility
+cd ${FINDER_APP_DIR}
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
+
+# Copy the finder related scripts and executables to the /home directory
+# on the target rootfs
+cp finder-test.sh finder.sh ${OUTDIR}/rootfs/home
+cp -rf conf/ ${OUTDIR}/rootfs/home
+cp writer ${OUTDIR}/rootfs/home
+cp autorun-qemu.sh ${OUTDIR}/rootfs/home
+
+# Chown the root directory
+cd ${OUTDIR}/rootfs
+sudo chown -R root:root *
+
+# Create initramfs.cpio.gz
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd ${OUTDIR}
+gzip -f initramfs.cpio
